41642c443906b17e35aab9c8d5d7b25d9e996cf2
Aolei <aolei.bao@accenture.com>
2020-08-11 10:46:30 +0800

    aug 11st

--- src/normalleetcode/Solution.java
+++ src/normalleetcode/Solution.java
@@ -2,7 +2,9 @@ package normalleetcode;
 
 import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Solution {
 
@@ -55,12 +57,66 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int []nums = {1,2,3,4,4,9,56,90};
-        int []r = s.twoSum(nums, 100);
-        for (int i : r) {
-            System.out.println(i);
+        char [][]board = {{'X', 'X', 'X', 'X'},{'X', 'O', 'O', 'X'},{'X', 'X', 'O', 'X'},{'X', 'O', 'X', 'X'}};
+        s.solve(board);
+        for (int i = 0; i < board.length; i++) {
+            for (char c : board[i]){
+                System.out.print(c + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    boolean isBorderPoint(int i, int j, int r, int c){
+        return i==0 || i == r-1 || j==0 || j == c-1;
+    }
+    boolean isValidPoint(int i, int j, int r, int c){
+        return i>=0 && i < r && j >=0 && j < c;
+    }
+
+    private final int [][]directions = {{1,0},{-1,0},{0,1},{0,-1}};
+    private void solveHelper(char[][] board, boolean[][]visited, int i, int j, int r, int c, List<int []> list, boolean[] flag){
+        visited[i][j] = true;
+        list.add(new int[]{i,j});
+        if (isBorderPoint(i,j,r,c)){
+            flag[0] = true;
+        }
+        for (int k = 0; k < 4; k++) {
+            int x = i + directions[k][0];
+            int y = j + directions[k][1];
+            if (isValidPoint(x,y,r,c) && board[x][y] == 'O' &&!visited[x][y]){
+                if (isBorderPoint(x,y,r,c)){
+                    flag[0] = true;
+                }
+                solveHelper(board,visited,x,y,r,c,list,flag);
+            }
         }
     }
+    public void solve(char[][] board) {
+        if(null == board || board.length == 0)
+            return;
+
+        int r = board.length;
+        int c = board[0].length;
+        boolean [][]visited = new boolean[r][c];
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (board[i][j] == 'O' && !visited[i][j]){
+                    boolean []flag = new boolean[1];
+                    List<int[]>list = new ArrayList<>();
+                    solveHelper(board,visited,i,j,r,c,list,flag);
+                    if (!flag[0]){
+                        for (int []a : list){
+                            board[a[0]][a[1]] = 'X';
+                        }
+                    }
+                }
+            }
+        }
+    }
+
 
 
 }
