9dae55f90e70f2d7752cfcee2c3035c03b2e5d35
Aolei <aolei.bao@accenture.com>
2020-08-12 18:32:53 +0800

    binary tree

--- src/binaryTree/Solution.java
+++ src/binaryTree/Solution.java
@@ -5,81 +5,60 @@ import java.util.Queue;
 import java.util.Stack;
 
 public class Solution {
+
+    private boolean hasPathSum(TreeNode node, int curSum, int sum){
+        if (node.left == null && node.right == null){
+            return curSum + node.val == sum;
+        }
+        curSum += node.val;
+
+        if (node.left != null && node.right != null){
+            return hasPathSum(node.left, curSum, sum) || hasPathSum(node.right, curSum, sum);
+        }else if (node.left != null){
+            return hasPathSum(node.left, curSum, sum);
+        }else{
+            return hasPathSum(node.right, curSum, sum);
+        }
+    }
     public boolean hasPathSum(TreeNode root, int sum) {
         if (null == root)
             return false;
-        Queue<TreeNode> queue = new ArrayDeque<>();
-        Queue<Integer> queueSum = new ArrayDeque<>();
-        queue.add(root);
-        queueSum.add(root.val);
-        while (!queue.isEmpty()){
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.remove();
-                int curSum = queueSum.remove();
-                if (null == node.left && null == node.right){
-                    if (curSum == sum){
-                        return true;
-                    }
-                }
-                if (null != node.left){
-                    int tempSum = curSum + node.left.val;
-                    queueSum.add(tempSum);
-                    queue.add(node.left);
-                }
-                if (null != node.right){
-                    int tempSum = curSum + node.right.val;
-                    queueSum.add(tempSum);
-                    queue.add(node.right);
-                }
-            }
-        }
-        return false;
-    }
-
-    public static void main(String[] args) {
-        TreeNode node1 = new TreeNode(5);
-        TreeNode node2 = new TreeNode(4);
-        TreeNode node3 = new TreeNode(8);
-        TreeNode node4 = new TreeNode(11);
-        TreeNode node5 = new TreeNode(13);
-        TreeNode node6 = new TreeNode(4);
-        TreeNode node7 = new TreeNode(7);
-        TreeNode node8 = new TreeNode(2);
-        TreeNode node9 = new TreeNode(1);
-
-        node1.left = node2;
-        node1.right = node3;
-        node2.left = node4;
-        node3.left = node5;
-        node3.right = node6;
-        node4.left = node7;
-        node4.right = node8;
-        node6.right = node9;
-
-        Solution s = new Solution();
-        System.out.println(s.hasPathSum(node1, 22));
+        return hasPathSum(root, 0, sum);
     }
 
     public int maxDepth(TreeNode root) {
-        if (null == root)
+        if(null == root){
             return 0;
-        Queue<TreeNode>queue = new ArrayDeque<>();
-        queue.add(root);
-        int count = 0;
-        while (!queue.isEmpty()){
-            count ++;
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.remove();
-                if (node.left !=null){
-                    queue.add(node.left);
-                }
-                if (node.right != null){
-                    queue.add(node.right);
-                }
-            }
         }
-        return count;
+        int left_ans = maxDepth(root.left);
+        int right_ans = maxDepth(root.right);
+        return Math.max(left_ans, right_ans) + 1;
     }
+
+    private boolean isSymmetricHelper(TreeNode node1, TreeNode node2){
+        if (null == node1 && null == node2){
+            return true;
+        }else if (node1 != null && node2 != null) {
+            return node1.val == node2.val && isSymmetricHelper(node1.left,node2.right)&&isSymmetricHelper(node1.right,node2.left);
+        }else{
+            return false;
+        }
+    }
+    public boolean isSymmetric(TreeNode root) {
+        return isSymmetricHelper(root,root);
+    }
+
+
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+
+
+        node1.left = node2;
+
+
+        Solution s = new Solution();
+        System.out.println(s.hasPathSum(node1, 1));
+    }
+
 }
